18880598f3dedc8ff05658aeec391e71985d1365
Shubham Dixit <51513309+shubhdev79@users.noreply.github.com>
2019-12-11 18:56:52 +0530

    Instances Configuration Files

--- Instance.tf
+++ Instance.tf
@@ -94,14 +94,21 @@ resource "aws_security_group" "Ansible_SecurityGroup" {
         from_port = 80
         to_port = 80
         protocol = "tcp"
-        cidr_blocks = ["${"0.0.0.0/0"}"]
+        cidr_blocks = ["${"X.X.X.X/32"}"]
     }
 
     ingress {
         from_port = 22
         to_port = 22
         protocol = "tcp"
-        cidr_blocks = ["${"14.98.201.90/32"}"]
+        cidr_blocks = ["${"X.X.X.X/32"}"]
+    }
+
+    ingress {
+        from_port = -1
+        to_port = -1
+        protocol = "icmp"
+        cidr_blocks = ["${"X.X.X.X/32"}"]
     }
     
     egress {
